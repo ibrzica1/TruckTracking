704d2505e6febb06ddf0218d84e9d463743faa32
ibrzica1 <igorbrzica1@gmail.com>
2026-04-06 10:51:25 +0200

    Create default value for status

---
+++ database/migrations/2026_04_06_084209_add_default_to_shipments_status.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Shipments;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('shipments', function (Blueprint $table) {
+            $table->string('status',10)
+            ->default(Shipments::UNNASIGNED)
+            ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('shipments', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
