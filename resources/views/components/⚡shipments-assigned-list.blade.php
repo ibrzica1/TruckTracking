3e17d7bcf67468fa96324cdf5df94a7d40bcb01e
ibrzica1 <igorbrzica1@gmail.com>
2026-04-27 17:27:54 +0200

    Add error display

--- resources/views/components/⚡shipments-assigned-list.blade.php
+++ resources/views/components/⚡shipments-assigned-list.blade.php
@@ -5,21 +5,31 @@ use Livewire\Component;
 new class extends Component
 {
     public int $count = 0;
+    public int $amount = 1;
 
     public function increment(){
-        $this->count++;
+        $this->count += $this->amount;
     }
 
     public function subtrack(){
-        if($this->count > 0){
-            $this->count--;
+        if($this->count - $this->amount > 0){
+            $this->count -= $this->amount;
+        }
+        else{
+            $this->addError('subtrack', 'Result cant be below 0');
         }
     }
 };
 ?>
 
 <div>
+    @error('subtrack')
+        <p>{{$message}}</p>
+    @enderror
+    
     <p>Clicked Times: {{$count}}</p>
     <button wire:click="increment">Add</button>
     <button wire:click="subtrack">Subtrack</button>
+    <input type="number" min=1 wire:model.live="amount">
+    <p>Amount: {{$amount}}</p>
 </div>
