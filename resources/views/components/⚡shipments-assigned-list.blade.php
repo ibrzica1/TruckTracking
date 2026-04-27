4ff40c70c191f49eb5125aa045b883e57964f83a
ibrzica1 <igorbrzica1@gmail.com>
2026-04-27 16:51:07 +0200

    Create functions increment and subtrack

--- resources/views/components/⚡shipments-assigned-list.blade.php
+++ resources/views/components/⚡shipments-assigned-list.blade.php
@@ -4,10 +4,22 @@ use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public int $count = 0;
+
+    public function increment(){
+        $this->count++;
+    }
+
+    public function subtrack(){
+        if($this->count > 0){
+            $this->count--;
+        }
+    }
 };
 ?>
 
 <div>
-    <p>Hello</p>
+    <p>Clicked Times: {{$count}}</p>
+    <button wire:click="increment">Add</button>
+    <button wire:click="subtrack">Subtrack</button>
 </div>
